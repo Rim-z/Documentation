556cefbf3989f306eecf384fbd1695984a17f0c2
Rim-z <zeayterrim6@gmail.com>
2022-12-11 17:47:12 +0200

    finish the basic queries

---
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -0,0 +1,12 @@
+A) Basic Queries 
+1-SELECT Name FROM students;
+2-SELECT * FROM students
+WHERE Age>'30';
+3-SELECT Name FROM students
+WHERE Gender='F' AND Age='30';
+4-SELECT Points FROM students
+WHERE name='Alex';
+5-ADD Myself as a new students
+INSERT INTO students VALUES ( '7' , 'Rim','23', 'F','700');
+6-UPDATE students SET Points='310' WHERE name='Basma';
+7-UPDATE students SET Points='150' WHERE name='Alex';
