fe3330327dd2fa784d5368bbd18d4139b40b8ae7
Rim-z <zeayterrim6@gmail.com>
2023-01-07 01:27:10 +0200

    changed app's name

--- 04_Challenges/NodeJS-Challenge/tasks.js
+++ 04_Challenges/NodeJS-Challenge/tasks.js
@@ -12,6 +12,7 @@
 function startApp(name){
   process.stdin.resume();
   process.stdin.setEncoding('utf8');
+  
   process.stdin.on('data', onDataReceived);
   console.log(`Welcome to ${name}'s application!`)
   console.log("--------------------")
@@ -79,4 +80,4 @@ function quit(){
 }
 
 // The following line starts the application
-startApp("Jad Sarout")
+startApp("Rim Zeayter")
