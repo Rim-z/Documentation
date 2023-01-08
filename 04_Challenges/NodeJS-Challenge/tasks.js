f14af961d666f431663a4388d212ac38fe0532c0
Rim-z <zeayterrim6@gmail.com>
2023-01-09 00:39:55 +0200

    documented extended hello

--- 04_Challenges/NodeJS-Challenge/tasks.js
+++ 04_Challenges/NodeJS-Challenge/tasks.js
@@ -78,7 +78,7 @@ function hello(value){
 
 // help show commands to use
 function help(){
-  console.log("lists of commands available: \n hello \n help \n unknown command \n exit \n quit \n node tasks.js  ")
+  console.log("lists of commands available: \n hello \n help \n unknown command \n exit \n quit \n node tasks.js \n Hello X")
 }
 
 /**
